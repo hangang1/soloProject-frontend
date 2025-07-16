c3e7c8bfef361b969094397d2d9906945a833f17
unknown <ldh9904@gmail.com>
2025-07-16 21:48:10 +0900

    feat: google Drive 문서 업로드 기능

--- utils/auth.js
+++ utils/auth.js
@@ -5,26 +5,19 @@ GoogleSignin.configure({
   scopes: [
     'profile',
     'email',
+    'https://www.googleapis.com/auth/drive.readonly',
     'https://www.googleapis.com/auth/drive.file',
     'https://www.googleapis.com/auth/documents',
   ],
 });
 
 export const loginWithGoogle = async () => {
-  try {
-    await GoogleSignin.hasPlayServices();
-    const userInfo = await GoogleSignin.signIn();
-    const tokens = await GoogleSignin.getTokens();
+  await GoogleSignin.hasPlayServices();
+  const userInfo = await GoogleSignin.signIn();
+  const tokens = await GoogleSignin.getTokens();
 
-    console.log('로그인 성공:', userInfo);
-    console.log('토큰:', tokens);
-
-    return {
-      accessToken: tokens.accessToken,
-      user: userInfo.user,
-    };
-  } catch (err) {
-    console.error('로그인 에러:', err);
-    throw new Error('Google 로그인에 실패했습니다.');
-  }
+  return {
+    accessToken: tokens.accessToken,
+    user: userInfo.user,
+  };
 };
